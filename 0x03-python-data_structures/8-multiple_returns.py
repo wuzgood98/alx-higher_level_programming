8958189ede11ef5e128207ad1c6a27c8b94e16c7
Gideon Addo <gideonaddoofori@gmail.com>
2023-09-08 22:15:43 +0000

    Completed task 8: More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        sentence[0] = None
-    return (len(sentence), sentence[0])
+    sen_len = len(sentence)
+
+    if sen_len == 0:
+        return  (sen_len, None)
+    return (sen_len, sentence[0])
