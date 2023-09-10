fb30a6b2142ec1e57263bd473232ba38f7c2f363
Gideon Addo <gideonaddoofori@gmail.com>
2023-09-10 09:19:54 +0000

    [FIX]: remove multiple space after return keyword

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     sen_len = len(sentence)
 
     if sen_len == 0:
-        return  (sen_len, None)
+        return (sen_len, None)
     return (sen_len, sentence[0])
