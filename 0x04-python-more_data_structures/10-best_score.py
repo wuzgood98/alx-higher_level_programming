aac7f387916fcd4d649f2f8e1189586c4f2f39fd
Gideon Addo <gideonaddoofori@gmail.com>
2023-09-13 17:12:24 +0000

    Refactor task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
-    scores = list(a_dictionary.values())
+    """scores = list(a_dictionary.values())
     highest = scores[0]
     for score in range(len(scores) - 1):
         if scores[score + 1] > highest:
@@ -10,4 +10,11 @@ def best_score(a_dictionary):
 
     for key, val in a_dictionary.items():
         if val == highest:
-            return (key)
+            return (key)"""
+    person = list(a_dictionary.keys())[0]
+    score = a_dictionary[person]
+    for key, val in a_dictionary.items():
+        if val > score:
+            score = val
+            person = key
+    return (person)
