56fe22a73b89abb0fc462ff7ef08165bb75e18b9
Gideon  Addo <gideonaddoofori@gmail.com>
2024-06-05 01:34:32 +0000

    Add click and turn red

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  // Add a click event handler to the DIV#red_header element
+  $('#red_header').click(function(){
+    // Update the text color of the <header> elements to red (#FF0000)
+    $(':header').css('color', '#FF0000')
+  })
+})
