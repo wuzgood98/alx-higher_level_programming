c9b9d7076fbe5d75f7c96d77a740ce0ba22ab0c0
Gideon  Addo <gideonaddoofori@gmail.com>
2024-06-05 02:29:41 +0000

    Add Change the text

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+// Add a click event handler to the DIV#update_header element
+$('#update_header').on('click', function(){
+  // Update the text of the <header> element to 'New Header!!!'
+  $('header').text('New Header!!!')
+})
