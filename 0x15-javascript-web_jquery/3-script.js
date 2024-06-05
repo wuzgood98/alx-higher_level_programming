41fdca048f9a1c2c2781dd69fe22d528e28c2042
Gideon  Addo <gideonaddoofori@gmail.com>
2024-06-05 01:44:24 +0000

    Add '.red' class

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  // Add a click event handler to the DIV#red_header element
+  $('#red_header').click(function(){
+    // Add class 'red' to the <header> element
+    $('header').addClass('red')
+  })
+})
