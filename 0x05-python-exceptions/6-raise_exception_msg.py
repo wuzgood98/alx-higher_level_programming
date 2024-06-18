c06d0eaba6b00aa8c13992901e54f6bffae0eb64
Gideon Addo <gideonaddoofori@gmail.com>
2024-06-18 22:31:15 +0000

    Add Raise a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Gideon O Addo
+
+def raise_exception_msg(message=""):
+    '''Raise a name exception with a message'''
+    raise NameError(message)
