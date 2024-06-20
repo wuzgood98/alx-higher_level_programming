22205e0cc3eea7f0c5a2463369d85fd8dd3366da
Gideon Addo <gideonaddoofori@gmail.com>
2024-06-20 16:51:14 +0000

    FIX: wrong output

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
+safe_function = __import__('101-safe_function').safe_function
 
-my_list = [1, 2, 3, 4, 5]
 
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+def my_div(a, b):
+    return a / b
+
+result = safe_function(my_div, 10, 2)
+print("result of my_div: {}".format(result))
+
+result = safe_function(my_div, 10, 0)
+print("result of my_div: {}".format(result))
+
+
+def print_list(my_list, len):
+    i = 0
+    while i < len:
+        print(my_list[i])
+        i += 1
+    return len
+
+result = safe_function(print_list, [1, 2, 3, 4], 10)
+print("result of print_list: {}".format(result))
