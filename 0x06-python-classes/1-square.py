eaf304f347f7f4d85e0b157cc92d4cfb125b041a
Gideon Addo <gideonaddoofori@gmail.com>
2024-06-20 16:18:30 +0000

    Add Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 1-square.py
+# Gideon O Addo
+"""Define a class Square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """Initialize a new Sqaure
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
