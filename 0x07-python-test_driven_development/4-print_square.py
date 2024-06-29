af994151cebb7a016e71d42204b12846ce9e81f7
Gideon Addo <gideonaddoofori@gmail.com>
2024-06-29 00:14:54 +0000

    Add Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# 4-print_square.py
+# Gideon O Addo
+"""Defines a square-printing function"""
+
+
+def print_square(size):
+    """Print a square with the character #.
+
+    Args:
+        size (int): The size length of the square
+
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than 0
+
+    Returns:
+        No return
+    """
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size <= 0:
+        raise TypeError('size must be >= 0')
+
+    for i in range(size):
+        [print('#', end='') for in range(size)]
+        print('')
