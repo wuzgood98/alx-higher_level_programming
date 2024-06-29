f738a9a0cdd3d2a2232ef012e018b6f40972a3ed
Gideon Addo <gideonaddoofori@gmail.com>
2024-06-29 01:29:10 +0000

    FIX: wrong exception

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -20,7 +20,7 @@ def print_square(size):
     if not isinstance(size, int):
         raise TypeError('size must be an integer')
     if size < 0:
-        raise TypeError('size must be >= 0')
+        raise ValueError('size must be >= 0')
 
     for i in range(size):
         [print('#', end='') for j in range(size)]
