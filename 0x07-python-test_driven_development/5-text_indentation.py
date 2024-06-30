730ee2f08b325a27c8421ec9dced2bd0ada021b7
Gideon Addo <gideonaddoofori@gmail.com>
2024-06-30 12:29:27 +0000

    Add Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# 5-text_indentation.py
+# Gideon O Addo
+"""Defines a function text_indetation"""
+
+
+def text_indentation(text):
+    """Print a text with 2 lines after eachh of these characters:
+    ., ?, :
+
+    Args:
+        text (str): The text to be processed
+
+    Raises:
+        TypeError: If text is not a string
+
+    Returns:
+        No return
+    """
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+
+    result = ''
+    i = 0
+    while i < len(text):
+        result += text[i]
+        if text[i] in '.?:':
+            result += '\n\n'
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
+
+    print(result, end='')
