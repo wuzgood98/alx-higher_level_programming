cbbad85c70b5492aba50708f59b224d311fd3f0b
Gideon Addo <gideonaddoofori@gmail.com>
2024-07-03 15:37:58 +0000

    CPython #3: Python Strings

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,33 @@
+/*
+ * File: 102-python.c
+ * Author: Gideon O Addo
+ */
+
+#include "Python.h"
+
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
+void print_python_string(PyObject *p)
+{
+	lont int length;
+
+	fflush(stdout);
+
+	printf("[.] string objext info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	length = ((PyASCIIObject *)(p))->length;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+}
