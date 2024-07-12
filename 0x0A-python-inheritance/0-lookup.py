65cc8800b9ecb0b3db4bd526c15800110726dd39
Gideon Addo <gideonaddoofori@gmail.com>
2024-07-12 11:21:08 +0000

    Add lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 0-lookup.py
+# Gideon O Addo
+"""Define a function lookup"""
+
+
+def lookup(obj):
+    """
+    Return the list of available attributes and methods
+    of an object.
+    """
+    return dir(obj)
