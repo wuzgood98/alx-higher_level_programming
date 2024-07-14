eeaa9fcf23edade7c9a50d8bee35dcaced12b889
Gideon Addo <gideonaddoofori@gmail.com>
2024-07-14 11:24:06 +0000

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+# Gideon O Addo
+"""Define an empty class"""
+
+
+class BaseGeometry:
+    """Base Geometry"""
+
+    def area(self):
+        """ Raise an exception. """
+        raise Exception('area() is not implemented')
