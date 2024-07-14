e83175b56d3d562c86c25e73bc6c88a7d6829482
Gideon Addo <gideonaddoofori@gmail.com>
2024-07-14 10:50:10 +0000

    FIX: pycodestyle type checking errors

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -16,6 +16,6 @@ def is_same_class(obj, a_class):
         True if the object is exactly an instance of the specified class,
         otherwise False.
     """
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     return False
