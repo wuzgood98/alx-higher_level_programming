20dd67ee980bdbb65c77d5486914625ee5ffffb5
Gideon Addo <gideonaddoofori@gmail.com>
2024-07-14 10:41:57 +0000

    Add Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# 2-is_same_class.py
+# Gideon O Addo
+"""Define a function is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Check if an object is exactly the same as an instance of a given class.
+    
+    Args:
+        obj (any): Object to compare with the class
+        a_class (type): class to compare with the object.
+
+    Returns:
+        True if the object is exactly an instance of the specified class,
+        otherwise False.
+    """
+    if type(obj) == a_class:
+        return True
+    return False
